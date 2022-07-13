60d3b366c1406b9449d619763bec9862bf1c8c24
子清 周 <17687952609@163.com>
2022-07-13 11:05:32 +0800

    update package

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,43 +1,44 @@
 <template>
-  <div>
-    <div>Bot昵称：{{ bot_name }}</div>
-    <div>Bot战力：{{ bot_rating }}</div>
-  </div>
-  <router-view></router-view>
-</template>
-
-<script>
-import $ from 'jquery';
-import { ref } from 'vue';
-
-export default {
-  name: "App",
-  setup: () => {
-    let bot_name = ref("");
-    let bot_rating = ref("");
-    
-    $.ajax({
-      url: "http://127.0.0.1:3000/pk/getBotInfo/",
-      type: "get",
-      success: resp => {
-        bot_name.value = resp.name;
-        bot_rating.value = resp.rating;
+    <div>
+      <div>Bot昵称：{{ bot_name }}</div>
+      <div>Bot战力：{{ bot_rating }}</div>
+    </div>
+    <router-view></router-view>
+  </template>
+  
+  <script>
+  import $ from 'jquery';
+  import { ref } from 'vue';
+  
+  export default {
+    name: "App",
+    setup: () => {
+      let bot_name = ref("");
+      let bot_rating = ref("");
+      
+      $.ajax({
+        url: "http://127.0.0.1:3000/pk/getBotInfo/",
+        type: "get",
+        success: resp => {
+          bot_name.value = resp.name;
+          bot_rating.value = resp.rating;
+        }
+      });
+  
+      return {
+          bot_name,
+          bot_rating,
       }
-    });
-
-    return {
-        bot_name,
-        bot_rating,
     }
   }
-}
-</script>
-
-<style>
-
-body {
-    background-image: url("@/assets/background.png");
-    background-size: cover;
-}
-
-</style>
+  </script>
+  
+  <style>
+  
+  body {
+      background-image: url("@/assets/background.png");
+      background-size: cover;
+  }
+  
+  </style>
+  
