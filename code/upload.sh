a073cc6fe2db9e9bb24edcad2f14341e4061bc28
子清 周 <17687952609@163.com~>
2023-02-05 17:58:24 +0800

    修改upload.sh

--- code/upload.sh
+++ code/upload.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 # 上传本地acapp端的内容
-find code/acapp/dist/js/*.js | xargs sed -i 's/(function(){var e={/const myfunc = (function(myappid, AcWingOS){var e={/g'
+find acapp/dist/js/*.js | xargs sed -i 's/(function(){var e={/const myfunc = (function(myappid, AcWingOS){var e={/g'
 
-find code/acapp/dist/js/*.js | xargs sed -i 's/AcWingOS:"AcWingOS"/AcWingOS:AcWingOS/g'
+find acapp/dist/js/*.js | xargs sed -i 's/AcWingOS:"AcWingOS"/AcWingOS:AcWingOS/g'
 
-find code/acapp/dist/js/*.js | xargs sed -i 's/.mount("#app")}()})();/.mount(myappid)}()});/g'
+find acapp/dist/js/*.js | xargs sed -i 's/.mount("#app")}()})();/.mount(myappid)}()});/g'
  
 echo "
 export class Game {
@@ -12,12 +12,12 @@ export class Game {
     	const myappid = '#' + id;
         myfunc(myappid, AcWingOS);
     }
-}" >> code/acapp/dist/js/*.js
+}" >> acapp/dist/js/*.js
 
-scp code/acapp/dist/js/*.js zzq:/home/zzq/king_of_bots/acapp
-scp code/acapp/dist/css/*.css zzq:/home/zzq/king_of_bots/acapp
+scp acapp/dist/js/*.js zzq:/home/zzq/king_of_bots/acapp
+scp acapp/dist/css/*.css zzq:/home/zzq/king_of_bots/acapp
 
 ssh zzq 'cd /home/zzq/king_of_bots/acapp && ./rename.sh'
 
 # 上传本地web端的内容
-scp code/web/dist/* zzq:/home/zzq/king_of_bots/web
+scp -r web/dist/* zzq:/home/zzq/king_of_bots/web
